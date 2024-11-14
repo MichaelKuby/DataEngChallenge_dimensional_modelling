2cac429cb79223b78a3eafb316b729fa30f25f32
Michael Kuby <m.kuby@kigroup.de>
2024-11-14 15:03:02 +0100

    Finding products that both men and women love.

--- src/SQL_transformations/get_products_men_and_women_love.py
+++ src/SQL_transformations/get_products_men_and_women_love.py
@@ -6,11 +6,9 @@ def get_products_men_and_women_love(dataframe_dict):
     _, favourite_mens_products = get_favourite_mens_product_df(dataframe_dict)
     _, favourite_womens_products = get_favourite_womens_product_df(dataframe_dict)
 
-    # convert favourite_womens_products and favourite_mens_products to sets
+    # convert favourite_womens_products and favourite_mens_products to sets and find their intersection
     favourite_womens_products = set(favourite_womens_products.index.tolist())
     favourite_mens_products = set(favourite_mens_products.index.tolist())
-
-    # find their intersection
     products = favourite_womens_products.intersection(favourite_mens_products)
 
     if len(products) == 0:
